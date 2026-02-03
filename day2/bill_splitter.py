d33a021eca5d0d6751628afa61001757cb4d5aba
bindhusudhakar <bindhusudhakar217@gmail.com>
2026-02-03 12:50:19 +0530

    day1 data science and AI/ML intership

---
+++ day2/bill_splitter.py
@@ -0,0 +1,8 @@
+total_bill = float( input ("Enter total bill amount: "))
+people = int(input("Enter number of people: "))
+share = total_bill / people
+print("total bill: ",total_bill)
+print("Each person pays: ",share)
+print(type(total_bill))
+print(type(people))
+print(type(share))
