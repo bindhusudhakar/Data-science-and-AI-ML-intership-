ab19adca87764ae049044db3cecdf82ecbb2fc0a
bindhusudhakar <bindhusudhakar217@gmail.com>
2026-02-18 17:07:53 +0530

    Data Science and AI/ML Intership

---
+++ day15/Fundamentals.py
@@ -0,0 +1,16 @@
+import random
+
+trials = 1000
+count_sum_7 = 0
+
+for _ in range(trials):
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    
+    if dice1 + dice2 == 7:
+        count_sum_7 += 1
+
+experimental_probability = count_sum_7 / trials
+
+print("Number of times sum was 7:", count_sum_7)
+print("Experimental Probability:", experimental_probability)
